44b593e4c061686f219cf7ad56eb0fcad09b57d5
deboraj-roy <deborajroy123@gmail.com>
2023-07-10 02:33:36 +0600

    Hello World

---
+++ Movie_store/Movie_store/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_store.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
