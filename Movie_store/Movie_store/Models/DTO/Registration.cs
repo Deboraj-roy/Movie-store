2aeb4311603ccdd75772459f0c86b4434286ef98
deboraj-roy <deborajroy123@gmail.com>
2023-07-10 17:48:39 +0600

    Registration class

---
+++ Movie_store/Movie_store/Models/DTO/Registration.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_store.Models.DTO
+{
+    public class Registration
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string UserNmae { get; set; }
+
+        [Required]
+        [RegularExpression("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; }
+
+        public string Role { get; set; }
+
+
+    }
+}
