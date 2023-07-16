bbf0e791b0ca4a7cabfbb5795ec1817b695b6f37
deboraj-roy <deborajroy123@gmail.com>
2023-07-17 01:57:44 +0600

    Movie and Genre Class added

---
+++ Movie_store/Movie_store/Models/Domain/MovieGenre.cs
@@ -0,0 +1,10 @@
+﻿namespace Movie_store.Models.Domain
+{
+    public class MovieGenre
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int GenreId { get; set; }
+
+    }
+}
