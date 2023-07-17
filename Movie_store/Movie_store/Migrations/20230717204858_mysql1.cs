5f8712040c61224ab28cd758fd96f274ab64d2fd
deboraj-roy <deborajroy123@gmail.com>
2023-07-18 02:50:43 +0600

    Mysql Database

---
+++ Movie_store/Movie_store/Migrations/20230717204858_mysql1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movie_store.Migrations
+{
+    public partial class mysql1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
